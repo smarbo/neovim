f08103134689b3c68086f1551c2fca33406a2534
Eddie Obrams <eddieobrams@gmail.com>
2024-06-02 01:06:06 +0100

    added nvim config

---
+++ lua/plugins/colors.lua
@@ -0,0 +1,2 @@
+return { "folke/tokyonight.nvim", name = "tokyonight",lazy=false, priority = 1000, config = function ()
+vim.cmd("colorscheme tokyonight") end}
---
+++ lua/plugins/lsp-config.lua
@@ -0,0 +1,35 @@
+return {
+	{
+		"williamboman/mason.nvim",
+		lazy = false,
+		config = function()
+			require("mason").setup({ PATH = "prepend" })
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		lazy = false,
+		config = function()
+			require("mason-lspconfig").setup({
+				ensure_installed = { "lua_ls", "tsserver" },
+				PATH = "prepend",
+			})
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		lazy = false,
+		config = function()
+			local lspconfig = require("lspconfig")
+			local capabilities = require("cmp_nvim_lsp").default_capabilities()
+			lspconfig.lua_ls.setup({ capabilities = capabilities })
+			lspconfig.tsserver.setup({
+				capabilities = capabilities,
+				cmd = { "typescript-language-server.cmd", "--stdio" },
+			})
+			vim.keymap.set("n", "K", vim.lsp.buf.hover, {})
+			vim.keymap.set("n", "gd", vim.lsp.buf.definition, {})
+			vim.keymap.set({ "n", "v" }, "<leader>ca", vim.lsp.buf.code_action, {})
+		end,
+	},
+}
